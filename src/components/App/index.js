017cedd54d13ac12a87351775057859c362caded
Aizhan Osmonova <aizhan.osmonova@endurance.com>
2019-04-03 15:31:52 -0400

    Implement navigation routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Navigation from '../Navigation';
 
 const App = () => (
-    <div>
-    </div>
+    <Router>
+        <Navigation />
+    </Router>
 );
 
 export default App;
