743cca671f0b0baef5f5c2fafd4645bd7c720ae6
Aizhan Osmonova <osmonova86@gmail.com>
2019-04-22 17:39:35 -0400

    Update Account page layout

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -7,8 +7,15 @@ import { withAuthorization } from '../Session';
 const AccountPage = () => (
     <div>
         <h1>Account Page</h1>
-        <PasswordForgetForm />
-        <PasswordChangeForm />
+
+        <div className="container">
+            <h2>Reset Password</h2>
+            <PasswordForgetForm />
+        </div>
+        <div className="container">
+            <h2>Change Password</h2>
+            <PasswordChangeForm />
+        </div>
     </div>
 );
 
